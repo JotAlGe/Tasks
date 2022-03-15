ca11dfa28c011493e15f472d5cc0e4a10372dc07
Juanchis <juanchismo10@gmail.com>
2022-03-15 19:29:12 -0300

    Comienzo del proyecto y configuración de bootstrap

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', 'Tasks')</title>
+
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        @livewireStyles
+    </head>
+    <body>
+        @yield('content')
+        <h1>
+            @yield('head', 'Encabezado')
+        </h1>
+        {{-- livewire --}}
+        @livewireScripts
+    </body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        @section('head', 'My head')
+    </div>
+@endsection
