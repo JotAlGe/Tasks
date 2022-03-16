1381e449c6ce219a2f87dedcf98be616cb888633
Juanchis <juanchismo10@gmail.com>
2022-03-15 23:54:49 -0300

    Mostrado de tareas en la vista del componente livewire

--- app/Http/Livewire/Tasks/Task.php
+++ app/Http/Livewire/Tasks/Task.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Tasks;
 
+use App\Models\Category;
+use App\Models\Priority;
+use App\Models\Task as ModelsTask;
 use Livewire\Component;
 
 class Task extends Component
 {
     public function render()
     {
-        return view('livewire.tasks.task');
+        return view('livewire.tasks.task', [
+            'tasks' => ModelsTask::all()
+        ]);
     }
 }
--- resources/views/livewire/tasks/task.blade.php
+++ resources/views/livewire/tasks/task.blade.php
@@ -1,41 +1,46 @@
 <div class="row">
+    @foreach ($tasks as $task)
+    {{-- {{ $task->id}}  - {{ $task->description }} --}}
     <div class="col-sm-6 col-xs-12">
         <div class="panel panel-default bg-success px-3 py-1">
             <div class="panel-body">
-                <h2 class="text-thin mt">Título de la tarea</h2>
+                <h2 class="text-thin mt text-white">{{ $task->description }}</h2>
                 <div class="clearfix">
                     <div class="pull-right">
-                        <ul class="list-inline m0">
-                        <li class="p0">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Follower" class="img-responsive img-circle thumb24">
-                            </a>
-                        </li>
-                        <li class="p0">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Follower" class="img-responsive img-circle thumb24">
-                            </a>
-                        </li>
-                        <li class="p0">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Follower" class="img-responsive img-circle thumb24">
-                            </a>
-                        </li>
-                        <li class="p0">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="Follower" class="img-responsive img-circle thumb24">
-                            </a>
-                        </li>
-                        <li class="p0">
-                            <a href="" class="v-middle">
-                                <strong>+7</strong>
-                            </a>
-                        </li>
-                        </ul>
+                        {{-- <ul class="list-inline m0">
+                            <li class="p0">
+                                <a href="">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Follower" class="img-responsive img-circle thumb24">
+                                </a>
+                            </li>
+                            <li class="p0">
+                                <a href="">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Follower" class="img-responsive img-circle thumb24">
+                                </a>
+                            </li>
+                            <li class="p0">
+                                <a href="">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Follower" class="img-responsive img-circle thumb24">
+                                </a>
+                            </li>
+                            <li class="p0">
+                                <a href="">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="Follower" class="img-responsive img-circle thumb24">
+                                </a>
+                            </li>
+                            <li class="p0">
+                                <a href="" class="v-middle">
+                                    <strong>+7</strong>
+                                </a>
+                            </li>
+                        </ul> --}}
+                        <small class="text-warning text-right">{{ $task->created_at->diffForHumans() }}</small>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    @endforeach
 </div>
 
